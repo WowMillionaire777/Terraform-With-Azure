acf7d82cddd3af893f17a5f91a62c8ee13072c7e
WowMillionaire777 <johnkenn777@gmail.com>
2022-12-06 16:31:43 -0500

    Update providers.tf

--- terraform2/Create-Multiple-Containers-Azure/providers.tf
+++ terraform2/Create-Multiple-Containers-Azure/providers.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "39be4fe7-7134-4527-ab9e-ef1e302f2d9d"
-  tenant_id       = "e4fa2e28-d8ff-4dde-a7c9-fe565d955f6e"
-  client_id       = "49bcca00-0071-4fb9-b9ad-9f3cd4f31224"
-  client_secret   = "J~w8Q~ILCtnlgIvwVj5f59lZhNd8gRmJ37Up7cVI"
+  subscription_id = ""
+  tenant_id       = ""
+  client_id       = ""
+  client_secret   = ""
   features {}
 }
